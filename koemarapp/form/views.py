ab61f9f474972784f79b7f83fb1f2a37d3d77031
Scottie Lee <scottie.c.f.lee@gmail.com>
2022-07-29 20:52:01 +0000

    added lines which make sure that the page works

--- koemarapp/form/views.py
+++ koemarapp/form/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
+
+def home(request):
+    context = {'name': 'Scottie'}
+    return render(request, 'form/home.html', context)
